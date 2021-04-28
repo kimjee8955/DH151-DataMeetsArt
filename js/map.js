2c508482cd8286de24e211b73a9f01d69e97b4ea
kimjee8955 <kimjee8955@gmail.com>
2021-04-27 23:24:26 -0700

    preliminary map

---
+++ js/map.js
@@ -0,0 +1,71 @@
+// Global variables
+let map;
+let lat = 34.0522;
+let lon = -118.2437;
+let zl = 3;
+let path = "data/art_loc.csv";
+let markers = L.featureGroup();
+
+// initialize
+$( document ).ready(function() {
+    createMap(lat,lon,zl);
+    readCSV(path);
+});
+
+// create the map
+function createMap(lat,lon,zl){
+	map = L.map('map').setView([lat,lon], zl);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
+
+// function to read csv data
+function readCSV(path){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+			console.log(data);
+			
+			// map the data
+			mapCSV(data);
+
+		}
+	});
+}
+
+function mapCSV(data){	
+	// loop through each entry
+	data.data.forEach(function(item,index){
+		// create marker
+		let marker = L.circleMarker([item.latitude,item.longitude])
+        .on('mouseover',function(){
+			this.bindPopup(`${item.title}<br><img src="${item.thumbnail_url}"><br>${item.latitude},${item.longitude}`).openPopup()
+		})
+
+		// add marker to featuregroup
+		markers.addLayer(marker)
+
+        // add entry to sidebar
+		$('.sidebar').append(`${item.title}<br><img src="${item.thumbnail_url}" onmouseover="flyToIndex(${index})"><br><br>`)
+	})
+
+	// add featuregroup to map
+	markers.addTo(map)
+
+	// fit markers to map
+	map.fitBounds(markers.getBounds())
+
+	//add button on map for default view
+	L.easyButton('fa-globe', function(btn,map){
+		map.fitBounds(markers.getBounds());
+    }, 	'default view').addTo(map);
+}
+
+function flyToIndex(index){
+	map.flyTo([item[index].lat,item[index].lon],12)
+	// open the popup
+	myMarkers.getLayers()[index].openPopup()	
+}
