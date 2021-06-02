cbc70ca18dec438c3fd17c0f6b92c28bc7811832
kimjee8955 <kimjee8955@gmail.com>
2021-06-01 17:27:06 -0700

    implement search bar

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,8 @@
 // Global variables
-let map;
-let lat = 0;
-let lon = 0;
-let zl = 3;
 let path = "data/Urban_Art.csv";
-let markers = L.featureGroup();
-
+let neighbor = [];
+let json_data;
+let query; 
 // initialize
 $( document ).ready(function() {
 	readCSV(path);
@@ -18,18 +15,19 @@ function readCSV(path){
 		download: true,
 		complete: function(data) {
 			console.log(data);
+			json_data = data;
 			
 			// display the data
-			    displayCSV(data);
+			displayCSV(data);
 		}
 	});
 }
 
 // function to display image and metadata
-function displayCSV(data){
+function displayCSV(){
     
 	// loop through each entry
-	data.data.forEach(function(item,index){
+	json_data.data.forEach(function(item,index){
 		// add entry to sidebar
         $('.sidebar').append(` 
             <div style="display: inline-block" class="img__wrap">
@@ -38,19 +36,38 @@ function displayCSV(data){
                     <p class="img__description"><b>Title:</b> ${item.title}<br><b>Artist(s):</b> ${item.artist_name}<br><b>Neighborhood:</b> ${item.County}<br><b>Year:</b> ${item.year}<br></p>
                 </div>
         </div>`)
+		// make an array of neighborhood and thumnail_url
+		neighbor.push(item.County);
 	})
     
 
 }
 
 function filterArt(){
-	const textBox = document.getElementById('searchbox');
-	let query = textBox.value;
+	const textBox = document.getElementById('searchBar');
+	query = textBox.value;
+	console.log(query);
     //Check if null or whitespace or empty search query
     if(!query || query.length === 0 || /^\s*$/.test(query)) {
-        textBox.value = "";
-        return;
-    }
+		$('.sidebar').replaceWith(`<p style="text-align:center">Please enter a valid neighborhood.</p>`);
+		return;
+    }else if(neighbor.includes(query)){
+		console.log("filtering...");
+		$('.sidebar').empty();
+		//filter data
+		filtered_data = json_data.data.filter(item => item.County === query);
+		filtered_data.forEach(function(item,index){
+			// add entry to sidebar
+			$('.sidebar').append(` 
+				<div style="display: inline-block" class="img__wrap">
+					<img class="img__img" src="${item.thumbnail_url}" width=300px height=330px/>
+					<div class="img__description_layer">
+						<p class="img__description"><b>Title:</b> ${item.title}<br><b>Artist(s):</b> ${item.artist_name}<br><b>Neighborhood:</b> ${item.County}<br><b>Year:</b> ${item.year}<br></p>
+					</div>
+			</div>`);
+		});
+	}
+	
 	
 }
 
